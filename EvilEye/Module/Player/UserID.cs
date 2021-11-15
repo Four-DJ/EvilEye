db60d677e2a7302d265bb045e3f17b1507c8225b
Joshua <73680704+UrFingPoor@users.noreply.github.com>
2021-11-15 00:31:18 -0500

    Add files via upload
    Copy UserID

---
+++ EvilEye/Module/Player/UserID.cs
@@ -0,0 +1,18 @@
+﻿using EvilEye.SDK;
+using System;
+
+namespace EvilEye.Module.World
+{
+    class CopyUserID : BaseModule
+    {
+        public CopyUserID() : base("Get User ID", "Copy the UserID to clipboard", Main.Instance.playerButton, null) { }
+
+        public override void OnEnable()
+        {
+            if (PlayerWrapper.GetUserID != "")
+                Misc.SetClipboard(PlayerWrapper.GetUserID);
+            LoggerUtill.Log("User ID: " + PlayerWrapper.GetUserID + " Copied to clipboard.", ConsoleColor.Green);
+        }
+
+    }
+}
