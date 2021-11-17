c42b36c8565561027fa2e38d5452664ac2b34cf7
Fish <discordisgay277@gmail.com>
2021-11-16 23:24:27 -0800

    Fixed Clear Console

--- EvilEye/Module/Settings/ConsoleClear.cs
+++ EvilEye/Module/Settings/ConsoleClear.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using EvilEye.SDK;
 
 namespace EvilEye.Module.Settings
 {
@@ -15,6 +16,18 @@ namespace EvilEye.Module.Settings
         public override void OnEnable()
         {
             Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("=============================================================================================================");
+            Console.WriteLine("                                                                                                             ");
+            Console.WriteLine("                          ███████╗██╗░░░██╗██╗██╗░░░░░  ███████╗██╗░░░██╗███████╗                            ");
+            Console.WriteLine("                          ██╔════╝██║░░░██║██║██║░░░░░  ██╔════╝╚██╗░██╔╝██╔════╝                            ");
+            Console.WriteLine("                          █████╗░░╚██╗░██╔╝██║██║░░░░░  █████╗░░░╚████╔╝░█████╗░░                            ");
+            Console.WriteLine("                          ██╔══╝░░░╚████╔╝░██║██║░░░░░  ██╔══╝░░░░╚██╔╝░░██╔══╝░░                            ");
+            Console.WriteLine("                          ███████╗░░╚██╔╝░░██║███████╗  ███████╗░░░██║░░░███████╗                            ");
+            Console.WriteLine("                          ╚══════╝░░░╚═╝░░░╚═╝╚══════╝  ╚══════╝░░░╚═╝░░░╚══════╝                            ");
+            Console.WriteLine("                                 Beta Release By Four_DJ, Literal, and Fish.                                 ");
+            Console.WriteLine("=============================================================================================================");
+            Console.ForegroundColor = ConsoleColor.White;
             LoggerUtill.Log("Cleared Console!", Console.ForegroundColor);
         }
     }
